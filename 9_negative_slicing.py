9b8bc2a4fef2310c7a32b8fa0919bdaff2353577
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 17:57:37 +0300

    Answered the 9th exercise questions

---
+++ 9_negative_slicing.py
@@ -0,0 +1,9 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+
+# Expected output:
+
+# ['h', 'i', 'j'] 
+
+print(letters[-4:-1])
+
+# conting from the last "h" is the -3 element so to print up to h we use -4
