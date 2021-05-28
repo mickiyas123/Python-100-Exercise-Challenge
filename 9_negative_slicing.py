231d49b3cda5e3065ab78d146475e64dca92282c
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 18:00:03 +0300

    added a second way of solving exercise 9

--- 9_negative_slicing.py
+++ 9_negative_slicing.py
@@ -5,5 +5,9 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 # ['h', 'i', 'j'] 
 
 print(letters[-4:-1])
+# or
+# print(letters[-3:])
 
 # conting from the last "h" is the -3 element so to print up to h we use -4
+
+# the second print statment is another easier alternative to print the answer
