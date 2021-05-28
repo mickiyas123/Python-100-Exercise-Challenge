ee4f0a75c9dc1cf27af45ae62bfaa59512eb8190
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 18:24:39 +0300

    Added A comment in the 1oth exercise file to explain what i did

--- 10_sequence_itempicking.py
+++ 10_sequence_itempicking.py
@@ -5,6 +5,8 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
 # ['a', 'c', 'e', 'g', 'i'] 
 print(letters[::2])
 
+# :: will navigate from starting of the lsit to the end and the 2 wiil choose element by 2 position difference
+
 
 
     
