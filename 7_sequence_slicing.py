3342415708b9317c2fb34bc8e997d1e057f93b20
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 17:41:41 +0300

    Answered the seventh exercise file

---
+++ 7_sequence_slicing.py
@@ -0,0 +1,7 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+# Expected output: 
+
+# ['a', 'b', 'c']
+print(letters[:3]) 
+
+# the print statment will print starting from the begining up to the the 3rd element but not the 3rd element.
