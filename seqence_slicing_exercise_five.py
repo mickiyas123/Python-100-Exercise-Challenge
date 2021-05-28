21bff70ca83108345a63a783fb5a45f7d740fc7f
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 17:31:05 +0300

    Answerd exercise six

---
+++ seqence_slicing_exercise_five.py
@@ -0,0 +1,5 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h" ,"i" , "j"]
+# Expected output
+# ['d','e','f']
+print(letters[3:6])
+# the 3rd element is d and to get 5th element we have to add one number so it will print it
