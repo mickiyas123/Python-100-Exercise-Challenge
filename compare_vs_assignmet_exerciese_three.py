9e80fe4683ecce56bb1ee1a24e7bb336b85cdf36
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 17:11:51 +0300

    removed some comment

--- compare_vs_assignmet_exerciese_three.py
+++ compare_vs_assignmet_exerciese_three.py
@@ -3,4 +3,4 @@ b = 2
 print(a == b)
 # print(b == c)
 # The first print statment will print false 
-# The second print statment will print Error because we don't have any variable called c
+# The second print statment will print Error because we don't have any variable called c.
