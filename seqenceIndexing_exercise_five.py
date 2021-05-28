18fd83ac9ed43854dbd0bd3edaf8fd6ea43ef0c6
mickiy123 <mikikinfe345@gmail.com>
2021-05-28 17:22:59 +0300

    Answered the fifth exercise file

---
+++ seqenceIndexing_exercise_five.py
@@ -0,0 +1,3 @@
+letters = ["a", "b", "c", "d", "e", "f", "g", "h" ,"i" , "j"]
+print(letters[1])
+# to print the second item which is b we access it by letters[1] because list counting starts from 0
