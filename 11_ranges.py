a1fb9c4b0bb2c2dd38940ffce200c82501b09508
mickiy123 <mikikinfe345@gmail.com>
2021-06-01 22:30:16 +0300

    Created and Answered thw 11th exercise

---
+++ 11_ranges.py
@@ -0,0 +1,6 @@
+my_range=range(1,21)
+my_list=list(my_range)
+print(my_list)
+# the first line will produce numbers starting from 1 upto 21 but not 21
+# the second line will convert the numbers into a list
+# the 3rd will print the list
