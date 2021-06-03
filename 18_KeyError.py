487899c47bebd3c1661f271c4b89a578391be67e
mickiy123 <mikikinfe345@gmail.com>
2021-06-03 22:18:52 +0300

    Created answered and explained exercise question number 18

---
+++ 18_KeyError.py
@@ -0,0 +1,7 @@
+d={"Name": "John", "Surname": "Smith"}
+
+# print(d["Smith"])
+# The print statment will print keyerror because we are trying to access dict by value instead of the key the correct print form to get Smith is
+print(d['Surname'])
+
+
