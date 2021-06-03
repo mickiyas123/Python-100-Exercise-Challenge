19bef4191380e74317488eab050b8e1c17bb65ab
mickiy123 <mikikinfe345@gmail.com>
2021-06-03 22:30:02 +0300

    Created exercise 19 file answered it and explained it

---
+++ 19_add_dict_key.py
@@ -0,0 +1,13 @@
+# Question: Add a new pair of key (e.g. c ) and value (e.g. 3 ) to the dictionary and print out the new dictionary.
+
+d = {"a": 1, "b": 2}
+
+# Expected output: 
+
+#   {'a': 1, 'c': 3, 'b': 2} 
+
+d['c']=3
+
+print(d)
+
+# we can add key and value to a dict by writing thd dictionary then the key and assign it the value we want
