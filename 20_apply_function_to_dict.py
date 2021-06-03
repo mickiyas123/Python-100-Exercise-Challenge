9e0edb8898a8c3f10095e28314f29347910d7e2d
mickiy123 <mikikinfe345@gmail.com>
2021-06-03 22:44:23 +0300

    Created exercise 20 file and solved it with explanation

---
+++ 20_apply_function_to_dict.py
@@ -0,0 +1,14 @@
+# Question: Calculate the sum of all dictionary values.
+
+d = {"a": 1, "b": 2, "c": 3}
+
+# Expected output: 
+
+#  6 
+
+# print(d.keys())
+print(sum(d.values()))   
+
+# .values gives us the value of the dictionary
+# we can also use .keys to find the keys
+
