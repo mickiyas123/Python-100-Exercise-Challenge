b28eca2fbfda04289f81ab17409ed8c62f9078fa
mickiy123 <mikikinfe345@gmail.com>
2021-06-04 20:27:38 +0300

    created solved and explained exercise number 26

---
+++ 26_print_range.py
@@ -0,0 +1,18 @@
+# Question: Make a script that prints out numbers from 1 to 10
+
+# Expected output:
+
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10
+
+for num in range(1,11):
+    print(num)
+# the range function will create a number list between 1 up to 11 but not 11
