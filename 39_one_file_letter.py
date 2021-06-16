107603d578684bf587d53b87b5dbfab571f024c5
mickiy123 <mikikinfe345@gmail.com>
2021-06-16 21:36:50 +0300

    Solved the question by another way which is including a folder for the 26 files i created

--- 39_one_file_letter.py
+++ 39_one_file_letter.py
@@ -1,8 +1,16 @@
-import string
+import string,os
 
 letters = string.ascii_lowercase
 
+if not os.path.exists("letters"):
+    os.mkdir("letters")
 for letter in letters:
-    with open(f'{letter}.txt','w') as file:
-        file.write(letter)
+    with open("letters/" + letter + '.txt','w') as file:
+        file.write(letter)   
+
+
+
+# for letter in letters:
+#     with open(f'{letter}.txt','w') as file:
+#         file.write(letter)
 
