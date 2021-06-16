69573d9fcacba6cd1ea2a7c6de6c502e9c088802
mickiy123 <mikikinfe345@gmail.com>
2021-06-16 22:12:28 +0300

    different approach for exercise 46

--- 46.py
+++ 46.py
@@ -1,9 +1,20 @@
-import os
+# import os
 
-files = os.listdir('letters')
+# files = os.listdir('letters')
+
+# letters = []
+# for file in files:
+#     with open(f'{file}','r') as file:
+#         letters.append(file.read())
+# print(letters)    
+
+import glob
 
 letters = []
-for file in files:
-    with open(f'{file}','r') as file:
+
+fileList = glob.glob("letters/*.txt")
+
+for fileName in fileList:
+    with open(fileName, "r") as file:
         letters.append(file.read())
-print(letters)    
+print(letters)        
