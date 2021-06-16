38b2526ceea7f4144f9758c5adc12a916c13dcc0
mickiy123 <mikikinfe345@gmail.com>
2021-06-16 22:36:17 +0300

    Created and solved exercise 49

---
+++ 49_pass.py
@@ -0,0 +1,6 @@
+# Question: The code is supposed to get some input from the user, but instead it produces an error. Please try to understand the error and then fix it.
+
+# pass = input("Please enter your password: ") 
+# pass can't be a variable name since it is reserved keyword
+
+pass1 = input("Please enter your password: ") 
