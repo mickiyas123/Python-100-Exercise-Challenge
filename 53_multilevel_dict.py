f637775e52123766405d3d5b7e9bd6fc0414c5b4
mickiy123 <mikikinfe345@gmail.com>
2021-06-18 01:09:57 +0300

    Created and solved exercise 53

---
+++ 53_multilevel_dict.py
@@ -0,0 +1,13 @@
+# Question: Print out the last name of the second employee.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+# Expected output: 
+
+# Smith  
+
+print(d['employees'][1]['lastName'])
