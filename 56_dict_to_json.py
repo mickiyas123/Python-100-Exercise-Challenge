1dd4c2f31062e71c4d5b35c955a3aa2e9981545e
mickiy123 <mikikinfe345@gmail.com>
2021-06-18 01:33:48 +0300

    Created and solved exercise 56

---
+++ 56_dict_to_json.py
@@ -0,0 +1,13 @@
+import json
+
+
+# Question: Store the dictionary in a json file.
+
+d = {"employees":[{"firstName": "John", "lastName": "Doe"},
+                {"firstName": "Anna", "lastName": "Smith"},
+                {"firstName": "Peter", "lastName": "Jones"}],
+"owners":[{"firstName": "Jack", "lastName": "Petter"},
+          {"firstName": "Jessy", "lastName": "Petter"}]}
+
+with open('company.json', 'w') as file:
+    json.dump(d, file, indent=4)
