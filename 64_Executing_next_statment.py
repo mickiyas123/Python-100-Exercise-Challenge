56c8a749cd3f98ad811f179b30927f9f4e59b2a1
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 16:14:11 +0300

    Wrote the question as a comment

---
+++ 64_Executing_next_statment.py
@@ -0,0 +1,9 @@
+# The following code prints Hello, check if 2 is greater than 1 and then breaks the loop because 2 is actuallygrater than 1. Therefore Hi is not printed out. Please replace break with something else so that Hello and Hi are printed out repeatedly.
+
+# while True:
+#     print("Hello")
+
+#     if 2 > 1:
+#         break
+#     print("Hi")
+
