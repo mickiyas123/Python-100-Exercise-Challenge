e49543535cf43543d5a11d0f2027a80054d3586c
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 21:03:38 +0300

    imported the question as a comment

---
+++ 68_user_friendly_translator.py
@@ -0,0 +1,10 @@
+# Question: Create an English to Portuguese translation program.
+
+# The program takes a word from the user as input and translates it using the following dictionary as a vocabulary source. In addition, return the message "We couldn't find that word!" when the user enters a word that is not in the dictionary. Also, make the program non case-sensitive meaning that for example, both earth and Earth should return the translation correctly for that word.
+
+# d = dict(weather = "clima", earth = "terra", rain = "chuva") 
+
+# Expected output: 
+
+# Enter word: hello
+# We couldn't find that word!
