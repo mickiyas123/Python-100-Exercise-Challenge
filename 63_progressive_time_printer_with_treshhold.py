2fde87b9ca30406e08bbb0346b19fad89c9d77ac
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 15:56:29 +0300

    imported the question and expected output in the form of comment

---
+++ 63_progressive_time_printer_with_treshhold.py
@@ -0,0 +1,9 @@
+# Question: Create a program that once executed the programs prints Hello  instantly first, then it prints it after 1 second, then after 2, 3, and then the program prints out the message "End of the Loop" and stops.
+
+# Expected output: 
+
+# Hello
+# Hello
+# Hello
+# Hello
+# End of Loop
