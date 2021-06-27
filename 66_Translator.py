018173920a06c2ad111068827745e224c5373ce3
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 17:13:38 +0300

    imported the question and expected output as a comment

---
+++ 66_Translator.py
@@ -0,0 +1,10 @@
+# Question: Create an English to Portuguese translation program.
+
+# The program takes a word from the user as input and translates it using the following dictionary as a vocabulary source.
+
+# d = dict(weather = "clima", earth = "terra", rain = "chuva") 
+
+# Expected output: 
+
+# Enter word: earth
+# terra
