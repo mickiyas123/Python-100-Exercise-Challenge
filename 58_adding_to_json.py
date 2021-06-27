d616c58ac3ff8294a6c3e1df8e23478f99bbd04b
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 13:49:03 +0300

    Imported the question as a code and wrote acode to display the image of the excpected otput

---
+++ 58_adding_to_json.py
@@ -0,0 +1,2 @@
+# Question: Please download the json file in the attachment and use Python to add a new employee to the content of the file so that the file looks like in the expected output below.
+
