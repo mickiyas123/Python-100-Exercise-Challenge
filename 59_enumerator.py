c628aaef6215ca9adf9fc96eef34b475aa8ff277
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 15:03:22 +0300

    Imported the question and excepected otput as acomment

---
+++ 59_enumerator.py
@@ -0,0 +1,9 @@
+# Question: Please complete the code so that it prints out the expected output.
+
+# a = [1, 2, 3] 
+
+# Expected output: 
+
+# Item 1 has index 0
+# Item 2 has index 1
+# Item 3 has index 2
