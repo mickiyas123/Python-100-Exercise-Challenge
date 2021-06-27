16a929d6d2707a8eb9f126acaff9ad6978a527d3
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 15:31:09 +0300

    imported the question and expected output in comment form

---
+++ 62_progressive_timed_printer.py
@@ -0,0 +1,12 @@
+# Question: Create a program that once executed the program prints Hello  instantly first, then it prints it after 1 second, then after 2, 3, 4, and so on the interval increases between prints.
+
+# Expected output: 
+
+# ...
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# Hello
+# ...
