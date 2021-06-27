6cc030c48cd1a460f365e720176923fa6e43b523
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 15:08:31 +0300

    Solved the question for the exercise 59 file

--- 59_enumerator.py
+++ 59_enumerator.py
@@ -7,3 +7,9 @@
 # Item 1 has index 0
 # Item 2 has index 1
 # Item 3 has index 2
+
+a = [1, 2, 3] 
+
+for index,value in enumerate(a):
+    # Enumerate function will go through both the index and the values
+    print(f'Item {index} has index {value}')
