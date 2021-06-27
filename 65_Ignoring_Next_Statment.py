f8010d7f25fab0db372a799112f424ad15d005bd
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 16:23:48 +0300

    the difference between the two question is this question wants the Hello to be printed out again and again

--- 65_Ignoring_Next_Statment.py
+++ 65_Ignoring_Next_Statment.py
@@ -1,4 +1,4 @@
-# The following code prints Hello, check if 2 is greater than 1 and then breaks the loop because 2 is actuallygrater than 1. Therefore Hi is not printed out. Please replace break with something else so that Hello and Hi are printed out repeatedly.
+# The following code prints Hello, check if 2 is greater than 1 and then breaks the loop because 2 is actuallygrater than 1. Therefore Hi is not printed out. Please replace break with something else so that Hello is printed out repeatedly.
 
 # while True:
 #     print("Hello")
@@ -7,10 +7,3 @@
 #         break
 #     print("Hi")
 
-while True:
-    print("Hello")
-
-    if 2 > 1:
-        pass
-    print("Hi")
-    # Using pass keyword it will pass to the next statment
