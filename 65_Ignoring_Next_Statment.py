ad130795c9127b3fc63691a6201cdc2cf1f4c09f
mickiy123 <mikikinfe345@gmail.com>
2021-06-27 16:21:43 +0300

    Copied the 64th question file because the 65th looks the same too

---
+++ 65_Ignoring_Next_Statment.py
@@ -0,0 +1,16 @@
+# The following code prints Hello, check if 2 is greater than 1 and then breaks the loop because 2 is actuallygrater than 1. Therefore Hi is not printed out. Please replace break with something else so that Hello and Hi are printed out repeatedly.
+
+# while True:
+#     print("Hello")
+
+#     if 2 > 1:
+#         break
+#     print("Hi")
+
+while True:
+    print("Hello")
+
+    if 2 > 1:
+        pass
+    print("Hi")
+    # Using pass keyword it will pass to the next statment
