dcb271f6a12cd06c3bc26cf179a9e0470b938cb3
mickiy123 <mikikinfe345@gmail.com>
2021-06-28 14:11:15 +0300

    Understood the solution it can be done using pandas and changing file format between csv and txt

--- 73_data_multiplier.py
+++ 73_data_multiplier.py
@@ -1,1 +1,9 @@
 # Multiply the values of the text file in the url by two and export the output to a new file
+
+import pandas as pd
+
+data = pd.read_csv("http://www.pythonhow.com/data/sampledata.txt")
+
+data_2 = data * 2
+
+data_2.to_csv("sapmledata_x_2.txt", index=None)
