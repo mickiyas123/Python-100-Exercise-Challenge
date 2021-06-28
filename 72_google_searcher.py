341922c19b9a26e0e802df9bbbe981cf7930a1b1
mickiy123 <mikikinfe345@gmail.com>
2021-06-28 13:55:52 +0300

    Understood how to solve the question and solved it using the module webbrowser

--- 72_google_searcher.py
+++ 72_google_searcher.py
@@ -1,2 +1,8 @@
 #Create a script that let the user type in a search term and opens and search on the browser for that term on google
 
+
+import webbrowser
+
+user_input = input("Enter Your google query: ")
+
+webbrowser.open('https://google.cpm/search?q=%s' % user_input)
