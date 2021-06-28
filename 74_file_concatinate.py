266e5036f94319b2bef70fdc19d9529f6cdf15c8
mickiy123 <mikikinfe345@gmail.com>
2021-06-28 15:05:04 +0300

    imported the question and excpected output as a comment

---
+++ 74_file_concatinate.py
@@ -0,0 +1,16 @@
+# Question: Please concatenate this file with this one to a single text file. The content of the output file should look like below.
+
+# Expected output: 
+
+# x,y
+# 3,5
+# 4,9
+# 6,10
+# 7,11
+# 8,12
+# 6,10
+# 8,18
+# 12,20
+# 14,22
+# 16,24
+
