5a2a19bf3577d9e728f85bc29a7127832af54f7a
mickiy123 <mikikinfe345@gmail.com>
2021-06-30 20:42:47 +0300

    Imported the question and excepected output as a commnet

---
+++ 77_Year_of_birth_calculator.py
@@ -0,0 +1,5 @@
+# Question: Create a script that asks the user to enter their age and the script calculates the user's year of birth and prints it out in a string like in the expected output. Please make sure you generate the current year dynamically.
+
+# Expected output: 
+
+# We think you were born back in 1988
