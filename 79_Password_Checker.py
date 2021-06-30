a3906f259fa0734d41d92331589be1bde0766ac5
mickiy123 <mikikinfe345@gmail.com>
2021-06-30 21:33:06 +0300

    Wrote the question for the exrecise

---
+++ 79_Password_Checker.py
@@ -0,0 +1,7 @@
+#Create a script that lets the user submit a password until they have satisfied three conditions:
+#1. Password contains atleast one number
+#2. Contains one uppercase letter
+#3. It is at least 5 chars long
+
+#print out message "Password is not fine" if the user didn't create a correct password
+
