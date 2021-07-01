9844d2ba09fbd810020b63f7ae8824da4d83a201
mickiy123 <mikikinfe345@gmail.com>
2021-07-01 13:58:56 +0300

    wrote and impotrted the question for the exercise

---
+++ 80_Advancde_Password_Checker.py
@@ -0,0 +1,5 @@
+#Create a script that lets the user submit a password until they have satisfied three conditions:
+#1. Password contains atleast one number
+#2. Contains one uppercase letter
+#3. It is at least 5 chars long
+# Give the exact reason why the user has not created a correct password
