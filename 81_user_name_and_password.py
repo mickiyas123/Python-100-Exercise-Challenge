7cb22d34edfad0641336e3696a38de7104637878
mickiy123 <mikikinfe345@gmail.com>
2021-07-01 17:33:18 +0300

    implemented a code that opens the file and do the rest of what is asked

--- 81_user_name_and_password.py
+++ 81_user_name_and_password.py
@@ -5,29 +5,53 @@
 # Give the exact reason why the user has not created a correct password
 # Before asking for password, ask for username
 
+# while True:
+#     user_names = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
+#     user_name = input("Enter User Name: ")
+#     if user_name in user_names:
+#         print("Username Exists")
+#     else:
+#         notes = []
+#         print("Username is fine")
+#         passwd = input("Enter password: ")
+#         if not any(char.isdigit() for char in passwd):
+#             notes.append("Password must contain atleast one digit")
+#         if not any(char.isupper() for char in passwd):
+#             notes.append("password must contain atleast one digit")
+#         if len(passwd) < 5:
+#             notes.append("password should be atleast 5 char long")
+#         if len(notes) == 0:
+#             print("Password is fine")
+#             break
+#         else:
+#             print("Please check the following:")
+#             for note in notes:
+#                 print(note)
+
+
+with open('users.txt' ,'r') as file:
+    names = [line.strip() for line in file]
 while True:
-    user_names = ['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune','Pluto']
-    user_name = input("Enter User Name: ")
-    if user_name in user_names:
-        print("Username Exists")
+    notes = []
+    user_name = input("Enter Username: ")
+    if user_name in names:
+        print("Username already exist") 
     else:
-        notes = []
-        print("Username is fine")
-        passwd = input("Enter password: ")
+        passwd = input("Enter Password: ")
         if not any(char.isdigit() for char in passwd):
-            notes.append("Password must contain atleast one digit")
-        if not any(char.isupper() for char in passwd):
-            notes.append("password must contain atleast one digit")
+            notes.append("Your password must contain atleast one digit")
+        if not any(char.isupper for char in passwd):
+            notes.append("Your password must contain atleast upercase letter")
         if len(passwd) < 5:
-            notes.append("password should be atleast 5 char long")
+            notes.append("you should have atleast 5 chars long")
         if len(notes) == 0:
-            print("Password is fine")
+            print("Password is safe")
             break
         else:
-            print("Please check the following:")
+            print("Please check the following ")
             for note in notes:
                 print(note)
-                
+
 
 
 
