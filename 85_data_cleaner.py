281c310dc947529dfdf4dc3f7089fe660e8303ef
mickiy123 <mikikinfe345@gmail.com>
2021-07-01 20:42:30 +0300

    imported the question and excpected ouput as a comment

---
+++ 85_data_cleaner.py
@@ -0,0 +1,13 @@
+# Question: Please download the attached countries-raw.txt file. The file contains the list of country names, but it also contains some unnecessary text among the countries. 
+
+# Please clean the list with Python by generating a new text file that contains a flawless list of country names without any other text or break lines in it. The new file content should look like in the expected output.
+
+# Expected output: 
+
+# Afghanistan
+# Albania
+# Algeria
+# Andorra
+# Angola
+# Antigua and Barbuda
+# Argentina ....
