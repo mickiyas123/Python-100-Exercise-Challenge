c7c382f4bb1e3f3facbfbdcdb34e2c78a7177bd6
mickiy123 <mikikinfe345@gmail.com>
2021-07-01 18:13:27 +0300

    understand the exercise

--- 82_Astronomy.py
+++ 82_Astronomy.py
@@ -4,4 +4,10 @@
 
 # 758085657.5026425
 
+import ephem
 
+jupiter = ephem.Jupiter()
+jupiter.compute('1230/1/1')
+distance_au_units = jupiter.sun_distance
+distance_km = distance_au_units * 149597870.691
+print(distance_km)
