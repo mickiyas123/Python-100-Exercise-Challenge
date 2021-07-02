c3dcab9dc353d4004465fed1254e0ccbfac9e063
mickiy123 <mikikinfe345@gmail.com>
2021-07-02 21:17:30 +0300

    implimented my solution

--- 87_Add_Missing_File.py
+++ 87_Add_Missing_File.py
@@ -2,3 +2,9 @@
 
 checklist = ["Portugal","Germany","Spain"]
 
+with open("countries_missing.txt" ,"r") as file:
+    content = file.readlines()
+content = [i.strip("\n") for i in content ]
+for item in checklist:
+    content.append(item)
+print(content)    
