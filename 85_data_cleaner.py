2c31f82f22dce28a7b9fc3040a0c499b1262b76e
mickiy123 <mikikinfe345@gmail.com>
2021-07-02 16:45:56 +0300

    corrected a spelling issue int he 85th python exercise file

--- 85_data_cleaner.py
+++ 85_data_cleaner.py
@@ -17,7 +17,7 @@ with open('countries_raw.txt' , 'r') as file:
     content = file.readlines()
 content = [i.strip("\n") for i in content if "\n" in i]
 content = [i for i in content if i !=""]
-content = [i for i in content if i !="Top of page"]
+content = [i for i in content if i !="Top of Page"]
 content = [i for i in content if len(i) != 1]
 
 # print(content)
