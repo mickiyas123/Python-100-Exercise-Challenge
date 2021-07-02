3cc88499cb420570349598534b959ead0fa7cde1
mickiy123 <mikikinfe345@gmail.com>
2021-07-02 16:36:09 +0300

    solved the questin using listcomphrension and with open

--- 85_data_cleaner.py
+++ 85_data_cleaner.py
@@ -11,3 +11,18 @@
 # Angola
 # Antigua and Barbuda
 # Argentina ....
+
+
+with open('countries_raw.txt' , 'r') as file:
+    content = file.readlines()
+content = [i.strip("\n") for i in content if "\n" in i]
+content = [i for i in content if i !=""]
+content = [i for i in content if i !="Top of page"]
+content = [i for i in content if len(i) != 1]
+
+# print(content)
+
+with open("countries.txt" ,'w') as file:
+    for i in content:
+        file.write(i+"\n")
+
