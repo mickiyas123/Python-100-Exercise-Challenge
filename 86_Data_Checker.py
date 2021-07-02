b5ab74fdd201c1008bd5124f5dea6a81bff1a714
mickiy123 <mikikinfe345@gmail.com>
2021-07-02 16:57:04 +0300

    solved the exercise challenge using the countries.txt file with with open and for loop

--- 86_Data_Checker.py
+++ 86_Data_Checker.py
@@ -3,3 +3,21 @@
 # checklist = ["Portugal", "Germany", "Munster", "Spain"]
 
 # One of the items is not a country. Please use Python and the file containing the list of countries (attached) as data source to filter out the checklist  of non-country items. Once you have filtered out checklist , then print it out.
+
+from typing import Coroutine
+
+
+checklist = ["Portugal", "Germany", "Munster", "Spain"]
+new_checklist = []
+
+with open('countries.txt' ,'r') as file:
+    lines = [line.strip() for line in file.readlines() ]  
+# print(lines)    
+
+for item in checklist:
+    if item not in lines:
+        continue
+    else:
+        new_checklist.append(item)
+print(new_checklist)        
+           
