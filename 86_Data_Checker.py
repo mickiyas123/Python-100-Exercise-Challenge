6e92ae833084ddb4af4b816693910703cc4997a3
mickiy123 <mikikinfe345@gmail.com>
2021-07-02 16:40:02 +0300

    imported the question and excepected output as comment

---
+++ 86_Data_Checker.py
@@ -0,0 +1,5 @@
+# Question: Please take a look at the following list:
+
+# checklist = ["Portugal", "Germany", "Munster", "Spain"]
+
+# One of the items is not a country. Please use Python and the file containing the list of countries (attached) as data source to filter out the checklist  of non-country items. Once you have filtered out checklist , then print it out.
