818f1c02876573090ec6293ba17da9093b9d4f3d
mickiy123 <mikikinfe345@gmail.com>
2021-07-04 18:40:17 +0300

    understood and remembered the solution using sqlite3

--- 89_Database_Filter.py
+++ 89_Database_Filter.py
@@ -1,2 +1,13 @@
 # Please download the database file database.db. The file contains a table with 50 country names along with their area in square km and population.
 # please use python to print out the countries that have an area of greater than 2,00,000
+
+import sqlite3
+
+conn = sqlite3.connect('database.db')
+cur = conn.cursor()
+cur.execute("SELECT  country FROM countries WHERE area >= 2000000")
+rows = cur.fetchall()
+conn.close()
+
+for i in rows:
+    print(i[0])
