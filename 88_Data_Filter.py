fb585e2a227748ea3cfef24aae6e1fdcf15bf579
mickiy123 <mikikinfe345@gmail.com>
2021-07-04 17:11:53 +0300

    Imported the question and excpected output as a comment

---
+++ 88_Data_Filter.py
@@ -0,0 +1,9 @@
+# Question: Create a script that uses the attached countries_by_area.txt  file as data source and prints out the top 5 most densely populated countries
+
+# Expected output: 
+
+# India
+# Pakistan
+# Nigeria
+# China
+# Indonesia
