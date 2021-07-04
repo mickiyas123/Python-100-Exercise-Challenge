98f5aba4088742ae87514a318668155834a45ac1
mickiy123 <mikikinfe345@gmail.com>
2021-07-04 18:14:57 +0300

    Understood the exercise answer

--- 88_Data_Filter.py
+++ 88_Data_Filter.py
@@ -21,3 +21,12 @@
 # countries = countries.sort_values('population_2013',ascending=False)
 # print(countries.head())
 
+import pandas as pd
+
+data = pd.read_csv('countries_by_area.txt')
+data['density'] = data['population_2013'] / data['area_sqkm']
+data  =data.sort_values(by='density',ascending=False)
+# print(data)
+for index, row in data[:5].iterrows ():
+    print(row["country"])
+
