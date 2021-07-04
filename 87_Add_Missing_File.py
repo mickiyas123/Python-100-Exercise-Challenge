e86c3b312d2de39a01b68adf9933ac291ce32ab3
mickiy123 <mikikinfe345@gmail.com>
2021-07-04 17:09:08 +0300

    removed 2 line of code with one for better solution

--- 87_Add_Missing_File.py
+++ 87_Add_Missing_File.py
@@ -1,16 +1,15 @@
 # Add the missing items to the file
 
 checklist = ["Portugal","Germany","Spain"]
+checklist = [i+ "\n" for i in checklist]
 
 with open("countries_missing.txt") as file:
     context = file.readlines()
 
-context = [i.strip() for i in context]
 
 for i in checklist:
     context.append(i)
 context = sorted(context)
-context = [i + "\n" for i in context]
 # print(context)
 
 with open("new_countries.txt", "w") as file1:
