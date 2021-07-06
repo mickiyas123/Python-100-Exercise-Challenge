b06217f6c102896ab4fcd7e8e8b5aabe45f2fa60
mickiy123 <mikikinfe345@gmail.com>
2021-07-06 15:46:18 +0300

    Solved the 95th python exercise using split and other methods

--- 95_comma_separated_input.py
+++ 95_comma_separated_input.py
@@ -1,2 +1,13 @@
 #create a program that asks the user to input values separated by commas and those values will be stored in a separate line each in a text file
 
+user_inputs = input("Enter Values: ")
+user_input = user_inputs.split(",")
+
+
+
+
+
+with open("user_data_commas.txt","a") as file:
+    for i in user_input:
+        file.write(i + "\n")
+    
