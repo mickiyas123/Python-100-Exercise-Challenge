9610fd6c65df67806217a57516651bb26554f282
mickiy123 <mikikinfe345@gmail.com>
2021-07-06 14:48:08 +0300

    Imported the question and execepcted output

---
+++ 93_Recursive_File_Counter.py
@@ -0,0 +1,5 @@
+# Question: Please download the attached ZIP file. Inside the ZIP file there's a directory named subdirs. That directory contains other directories inside. Please write a script that counts the number of .py files contained inside subdirs and all its sub-directories.
+
+# Expected output: 
+
+# 3
