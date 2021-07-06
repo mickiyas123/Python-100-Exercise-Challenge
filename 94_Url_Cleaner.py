b1b86c5ee9876c474a07ec121ec43bf808292b5d
mickiy123 <mikikinfe345@gmail.com>
2021-07-06 14:58:57 +0300

    Imported the 94th python exercise file

---
+++ 94_Url_Cleaner.py
@@ -0,0 +1,16 @@
+# Question: Please download the attached urls.txt file. The file contains some broken URLs. Here's what the file contains:
+
+# https:/www.google.com
+# https:/www.yahoo.com
+# https:/www.stackoverflow.com
+# https:/www.pythonhow.com
+
+# Please use Python to remove the "s" from "https" and add another forward slash next to the existing slash, so the content looks like in the expected output.
+
+# Expected output: 
+
+# http://www.google.com
+# http://www.yahoo.com
+# http://www.stackoverflow.com
+# http://www.pythonhow.com
+
