b62cd038c285c9204a695d91c389e34dc7f53887
mickiy123 <mikikinfe345@gmail.com>
2021-07-06 13:57:24 +0300

    imported the question and excepected output as a comment

---
+++ 92_file_counter.py
@@ -0,0 +1,5 @@
+# Question: Please download the attached ZIP file and extract its files in a folder. Then, write a script that counts and prints out the number of .py files in that folder.
+
+# Expected output: 
+
+# 2
