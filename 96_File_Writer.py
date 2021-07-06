61e46875714e8670dba2fd3b9111a9861c00662c
mickiy123 <mikikinfe345@gmail.com>
2021-07-06 16:08:56 +0300

    solved the 96th python exercise file with loops and input statment

--- 96_File_Writer.py
+++ 96_File_Writer.py
@@ -1,2 +1,17 @@
 # Create a program that asks the user to submit text repeatedly
 #the program closes when the user submit ClLOSE
+
+
+while True:
+    user_inputs = input("Write a Value: ")
+    if user_inputs == "CLOSE":
+        break
+    else:
+         with open("user_data.txt" ,"a") as file:
+            for user_input in user_inputs:
+                file.write(user_input + "\n")
+
+    
+    
+    
+   
